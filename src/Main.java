69b577edf97d3ad8e02480f6bf94d0d53b50a74e
amit6june <kumar.amit.mishra@gmail.com>
2023-02-24 12:48:58 +1100

    Feature2 (#3)
    * added hello-wor
    
    * added hello-wor

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
         System.out.println("Hello wor-from branch");
         System.out.println("Hello wor! directly in main");
         System.out.println("Hello word againnndirectly in main");
+
+        System.out.println("brnahc 1 Hello word againnndirectly in main");
+        System.out.println("brnahc 2 Hello word againnndirectly in main");
+
         System.out.println("Hello word againnndirectly in and more main");
+
 
     }
 }
